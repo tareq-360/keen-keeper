62210a3851fbbdaa65fe46c0492834ec1f761dbd
tareq-360 <mdtareqhossain2002@gmail.com>
2026-07-07 21:08:39 +0600

    details page design  done

---
+++ src/app/details/page.jsx
@@ -0,0 +1,78 @@
+import { Archive, BellRing, MessageSquare, Phone, Trash2, Video } from "lucide-react";
+import Image from "next/image";
+
+const Details = () => {
+    return (
+        <div className=" flex flex-wrap justify-center py-10 bg-[#F8FAFC] text-black items-center ">
+
+            {/* Left div */}
+            <div className=" text-center p-5 rounded-md">
+                <div className="bg-[#ffffff] p-5 px-10 space-y-2 mb-3">
+                    <Image className=" rounded-full mx-auto" src="https://i.ibb.co.com/Ywp83WX/image.jpg" alt="Profile image" width={80} height={80}></Image>
+                    <h3 className="text-2xl pt-2">Name Here</h3>
+                    <p className=" bg-red-400 rounded-2xl">Status</p>
+                    <p className=" bg-blue-400 rounded-2xl">Tags</p>
+                    <span className="text-sm">Lorem ipsum dolor sit amet <br></br> consectetur adi</span>
+                </div>
+                <div className="  space-y-2 ">
+                    <div className="bg-[#ffffff] p-2 rounded-md flex items-center justify-center gap-2">
+                        <BellRing width={20} height={20}/>
+                        <p >Snooze 2 Weeks</p>
+                    </div>
+                    <div className="bg-[#ffffff] p-2 rounded-md flex items-center justify-center gap-2">
+                        <Archive width={20} height={20}/>
+                        <p >Archive</p>
+                    </div>
+                    <div className="bg-[#ffffff] p-2 rounded-md flex items-center justify-center text-red-500 gap-2">
+                        <Trash2 width={20} height={20} />
+                        <p >Delete</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Right Div */}
+            <div className="space-y-5">
+                <div className=" flex gap-5 rounded-md">
+                    <div className="bg-[#ffffff] p-5">
+                        <p>62</p>
+                        <p>Days Since Contact</p>
+                    </div>
+                    <div className="bg-[#ffffff] p-5">
+                        <p>30</p>
+                        <p>Goal (Days)</p>
+                    </div>
+                    <div className="bg-[#ffffff] p-5">
+                        <p>Feb 27,2026</p>
+                        <p>Next Due</p>
+                    </div>
+                </div>
+                <div className="bg-[#ffffff] p-5 rounded-md">
+                    <div className=" flex justify-between">
+                        <p>Relationship Goal</p>.
+                        <p className="btn">Edit</p>
+                    </div>
+                    <p>Connect every<span className=" font-bold">30 Days</span></p>
+                </div>
+                <div className="bg-[#ffffff] p-5 rounded-md space-y-5">
+                    <p>Quick Check-in</p>
+                    <div className=" flex gap-5 justify-between">
+                        <div className=" flex flex-col items-center justify-center">
+                            <Phone />
+                            <p>Call</p>
+                        </div>
+                        <div className=" flex flex-col items-center justify-center">
+                            <MessageSquare />
+                            <p>Text</p>
+                        </div>
+                        <div className=" flex flex-col items-center justify-center">
+                            <Video />
+                            <p>Video</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Details;
--- src/app/hero/page.jsx
+++ src/app/hero/page.jsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 import { Plus } from "lucide-react";
 import contacts from '../../../public/data/contacts.json';
 import Image from "next/image";
@@ -18,7 +18,7 @@ const Hero = async () => {
                 </p>
                 <button className="btn bg-[#244D3F]">+Add a Friend</button>
             </div>
-            <div className=" grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 text-center justify-items-center pt-5">
+            <div className=" grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4 text-center justify-items-center pt-5 gap-5">
                 <div className=" bg-[#FFFFFF] w-40 h-25 flex flex-col items-center justify-center p-3" >
                     <h3>{contacts.length}</h3>
                     <p className=" text-sm">Total Friend</p>
@@ -38,25 +38,27 @@ const Hero = async () => {
             </div>
             <div className="pt-10">
                 <h3>Friends</h3>
-                <div className="bg-[#F8FAFC] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                    {contacts.map(contact => <div key={contact.id} className="card   shadow-sm bg-[#ffffff]">
-                        <figure className="px-10 pt-10">
-                            <Image className=" rounded-full"  src={contact.picture} alt={contact.name} width={80} height={80}></Image>
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{contact.name}</h2>
-                            <p>{contact.days_since_contact}d ago</p>
-                            <div className="card-actions flex flex-col items-center">
-                                <div className=" space-x-2">
-                                    {contact.tags.map((tag , idx) => <button className="p-2 rounded-2xl bg-[#CBFADB]" key={idx}>{tag}</button>)}
+                <Link href="/details">
+                    <div className="bg-[#F8FAFC] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                        {contacts.map(contact => <div key={contact.id} className="card   shadow-sm bg-[#ffffff]">
+                            <figure className="px-10 pt-10">
+                                <Image className=" rounded-full" src={contact.picture} alt={contact.name} width={80} height={80}></Image>
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{contact.name}</h2>
+                                <p>{contact.days_since_contact}d ago</p>
+                                <div className="card-actions flex flex-col items-center">
+                                    <div className=" space-x-2">
+                                        {contact.tags.map((tag, idx) => <button className="p-2 rounded-2xl bg-[#CBFADB]" key={idx}>{tag}</button>)}
+                                    </div>
+                                    <button className={`btn btn-sm rounded-2xl ${contact.status === "overdue" ? "bg-red-500" : contact.status === "on-track" ? " bg-green-700" : " bg-yellow-600"}`}>{contact.status}</button>
                                 </div>
-                                <button className={`btn btn-sm rounded-2xl ${contact.status==="overdue"? "bg-red-500" : contact.status==="on-track"? " bg-green-700" : " bg-yellow-600"}`}>{contact.status}</button>
                             </div>
-                        </div>
-                    </div>)}
+                        </div>)}
 
 
-                </div>
+                    </div>
+                </Link>
             </div>
         </div>
     );
